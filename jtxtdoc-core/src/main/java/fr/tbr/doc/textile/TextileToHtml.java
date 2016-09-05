7955a83d5ea4b910e6f7ed9e502ea6ffb556e659
tbrou <tbrou@tbrou-PC7.natsystem.fr>
2016-09-05 09:59:30 +0200

    * add : parser from textile to html

---
+++ jtxtdoc-core/src/main/java/fr/tbr/doc/textile/TextileToHtml.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package fr.tbr.doc.textile;
+
+import java.io.StringWriter;
+
+import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
+import org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder;
+import org.eclipse.mylyn.wikitext.core.parser.markup.MarkupLanguage;
+
+/**
+ * @author tbrou
+ *
+ */
+public class TextileToHtml {
+	
+	
+	public String parse(MarkupLanguage language, String wikiText) {
+
+		StringWriter writer = new StringWriter();
+		HtmlDocumentBuilder builder = new HtmlDocumentBuilder(writer);
+		MarkupParser parser = new MarkupParser(language, builder);
+		parser.parse(wikiText);
+		return writer.toString();
+	}
+
+
+	
+}
